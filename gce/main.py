04e021b4e5fdd36290012c011bffb4a4cf6f9c72
Jaime Luengo <jaimeluengo@macOS.local>
2020-11-05 08:58:58 -0800

    Parameterized wait time

--- gce/main.py
+++ gce/main.py
@@ -20,30 +20,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world_handler():
-    logging.error('Received call to hello_world_handler.')
+    logging.info('Received call to hello_world_handler.')
     return 'Hello world!'
 
-@app.route('/1mins', methods=['GET'])
-def one_minute_handler():
-    logging.error('Received call for 1 minute time request.')
-    time.sleep(60*1)
-    logging.error('Waited successfully 1 minute.')
-    return "After waiting 1 mins it still worked!"
-
-@app.route('/3mins', methods=['GET'])
-def three_minute_handler():
-    logging.error('Received call for 3 minute time request.')
-    time.sleep(60*3)
-    logging.error('Waited successfully 3 minute.')
-    return "After waiting 3 mins it still worked!"
-
-@app.route('/4mins', methods=['GET'])
-def four_minute_handler():
-    logging.error('Received call for 4 minute time request.')
-    time.sleep(60*4)
-    logging.error('Waited successfully 4 minute.')
-    return "After waiting 4 mins it still worked"
-
+@app.route('/<int:num_minutes>', methods=['GET'])
+def one_minute_handler(num_minutes):
+    logging.info('Received call for '+num_minutes+' min time request.')
+    time.sleep(60*num_minutes)
+    logging.info('Waited successfully '+num_minutes+' mins.')
+    return 'After waiting '+num_minutes+' mins it still worked!'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
